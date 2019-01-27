507c356cf54d6cb359c38ee8e0f7a5e6422a1c8e
Rocky Singh <rockygsingh@icloud.com>
2019-01-27 14:22:37 -0500

    rendered app name

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from '.src/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('auth', () => App);
