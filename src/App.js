f531a1db44969229dc282e146aeb397235f9aa90
Rocky Singh <rockygsingh@icloud.com>
2019-01-27 20:50:49 -0500

    config firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './component/common';
+import LoginForm from './component/LoginForm';
 
 class App extends Component {
+
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDyfDmzyS7PBf7yGMCPOk6zvLbOsnyW21I',
+      authDomain: 'auth-fc091.firebaseapp.com',
+      databaseURL: "https://auth-fc091.firebaseio.com",
+      projectId: "auth-fc091",
+      storageBucket: "auth-fc091.appspot.com",
+      messagingSenderId: "1018571346490"
+    });
+  }
+
+
+
   render() {
     return (
       <View>
       <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
