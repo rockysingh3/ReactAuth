5a0229596155597afa4eecebd66500951afcf7b6
Rocky Singh <rockygsingh@icloud.com>
2019-01-27 21:34:54 -0500

    added state to handle user input

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,18 +1,36 @@
-import React { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import React, { Component } from 'react';
+import { Button, Card, CardSection, Input } from './common';
 
 
 class LoginForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  // gets user input and stores it in the state
+  handleChangeText = (typedText) => {
+    this.setState({ text: typedText });
+  }
+
+
   render() {
     return (
       <Card>
         <CardSection />
+          <Input
+            label="Email"
+            value={this.state.text}
+            onChangeText={this.handleChangeText}
+           />
         <CardSection />
 
         <CardSection>
-          <Button></Button>
-          </CardSection>
+            <Button>
+              Log in
+            </Button>
+        </CardSection>
       </Card>
     );
   }
